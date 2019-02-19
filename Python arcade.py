c0a92b829822298ad6e95730a64660063e326096
Foxo123 <32681514+Foxo123@users.noreply.github.com>
2019-02-19 10:30:20 +0100

    Update Python arcade.py

--- Python arcade.py
+++ Python arcade.py
@@ -20,13 +20,18 @@ class Game(arcade.Window):
 
     def setup(self):
         # Create your sprites and sprite lists here
-        pass
+        rondje1 = None
 
 
     def rondje(self, x,y,radius,color):
         arcade.draw_circle_filled(x,y,radius,color)
 
-    rondje1 = None
+    pos_x = 420
+    pos_y = 100
+
+    momentum_x = 0
+    momentum_y = 0
+
     def on_draw(self):
         """
         Rendering function
@@ -35,7 +40,9 @@ class Game(arcade.Window):
         # This command should happen before we start drawing. It will clear
         # the screen to the background color, and erase what we drew last frame.
         arcade.start_render()
-        rondje1 = self.rondje(300,300,200,arcade.color.WHITE)
+        self.rondje1 = self.rondje(self.pos_x,self.pos_y,30,arcade.color.WHITE)
+        #self.niggatoilet = arcade.
+
         # Call draw() on all your sprite lists below
 
     def update(self, delta_time):
@@ -44,22 +51,39 @@ class Game(arcade.Window):
         Normally, you'll call update() on the sprite lists that
         need it.
         """
-        pass
+        if self.momentum_y == 1:
+            self.pos_y += 5
+        if self.momentum_y == -1:
+            self.pos_y += -5
+        ##self.pos_x += 5
+        if self.pos_x > SCREEN_WIDTH:
+            self.pos_x = 0
+        if self.pos_x < -10:
+            self.pos_x = 799
+        
+        
 
     def on_key_press(self, key, key_modifiers):
-        """
-        Called whenever a key on the keyboard is pressed.
+        if key == 119:
+            self.momentum_y = 1
+        if key == 115:
+            self.momentum_y = -1
+        if key == 97:
+            self.momentum_x = 1
+        if key == 100:
+            self.momentum_x = -1
 
-        For a full list of keys, see:
-        http://arcade.academy/arcade.key.html
-        """
-        pass
+            
 
     def on_key_release(self, key, key_modifiers):
-        """
-        Called whenever the user lets off a previously pressed key.
-        """
-        pass
+        if key == 119:
+            self.momentum_y = 0
+        if key == 115:
+            self.momentum_y = 0
+        if key == 97:
+            self.momentum_x = 0
+        if key == 100:
+            self.momentum_x = 0
 
     def on_mouse_motion(self, x, y, delta_x, delta_y):
         """
